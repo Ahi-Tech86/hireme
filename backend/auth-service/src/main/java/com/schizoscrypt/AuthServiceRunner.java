d3e4c607822d14e1032982e847347975d6a72100
Ahi-Tech86 <ahi.tech86@gmail.com>
2024-09-03 14:10:02 +0300

    [Backend] {EurekaServer}: created eureka server and connected authService to EurekaServer

--- backend/auth-service/src/main/java/com/schizoscrypt/AuthServiceRunner.java
+++ backend/auth-service/src/main/java/com/schizoscrypt/AuthServiceRunner.java
@@ -3,7 +3,9 @@ package com.schizoscrypt;
 import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class AuthServiceRunner {
     public static void main(String[] args) {
